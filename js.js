dd1fa0a192f18d0f2d062a089610e4e47c829c1c
123456789afruz <afruzoxunov@gmail.com>
2024-09-27 19:04:03 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,47 @@
+const coin = document.querySelector('#coin')
+const scoreEl = document.querySelector('#score')
+const buyUpgrade = document.querySelector('#buyUpgrade')
+const message = document.querySelector('#message')
+const buyBoost = document.querySelector('#buyBoost')
+
+let score = 0
+let clickValue = 1
+let upgradeCost = 100
+let boostCost = 50
+let boostDuration = 5000
+
+coin.addEventListener('click', () => {
+   score += clickValue
+   scoreEl.innerText = score
+})
+
+buyUpgrade.addEventListener('click', () => {
+   if(score >= upgradeCost) {
+      score -= upgradeCost
+      scoreEl.innerText = score
+      clickValue++
+      upgradeCost = Math.floor( upgradeCost * 1.5)
+      buyUpgrade.innerText = `Купить улучшение (${upgradeCost} коинов)`
+      message.innerText = 'Вы успешно купили улучшение👍'
+   } else {
+      message.innerText = 'На балансе недостаточно очков😢'
+
+   }
+})
+
+buyBoost.addEventListener('click', () => {
+   if (score >= boostCost) { 
+      score -= boostCost
+      clickValue *= 2
+      message.innerText = 'Вы успешно купили буст👍'
+      setTimeout(() => {
+         clickValue = 1
+         message.innerText = 'Буст закончился😢'
+         
+      }, boostDuration);
+      
+   }  else {
+      message.innerText = 'На балансе недостаточно очков для буста😢'
+
+   }
+})
